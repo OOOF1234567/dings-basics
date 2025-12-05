b4d8c698d149136eb1f4a7672738d2e3e6dc11e1
OOOF1234567 <giocarone244@gmail.com>
2025-12-05 18:03:41 -0500

    Add files via upload

---
+++ dings basics.py
@@ -0,0 +1,13 @@
+#dings basics
+
+a==input("do you want to ding y/n")
+if a=="y":
+ print("correct")
+if a=="n":
+ print("incorrect")
+b==input("does a GEEBS ding y/n/sometimes")
+if b=="sometimes":
+ print("correct")
+else:
+ print(incorrect")
+
